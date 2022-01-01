472c9a4ed7d541efbae43cb3784e629531d7a4ad
unknown <korbar41@gmail.com>
2022-01-01 09:48:17 +0100

    second commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '@styles/globals.css'
 
+const Noop = ({children}) => <>{children}</> // takes the children and re-render them
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const Layout = Component.Layout ?? Noop // if Layout is undefined (null) => Noop
+
+  return (
+  <Layout>
+
+    <Component {...pageProps} />
+
+  </Layout>
+
+  )
 }
 
 export default MyApp
